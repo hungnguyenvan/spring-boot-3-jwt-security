02784fc69e3701edf8ca0228ece007724709ab1a
hungnguyenvan <hung29091987@gmail.com>
2025-08-02 22:20:11 +0700

    update

--- src/main/java/com/alibou/security/config/JwtService.java
+++ src/main/java/com/alibou/security/config/JwtService.java
@@ -56,6 +56,9 @@ public class JwtService {
           UserDetails userDetails,
           long expiration
   ) {
+    // Add authorities to the token claims
+    extraClaims.put("authorities", userDetails.getAuthorities());
+    
     return Jwts
             .builder()
             .setClaims(extraClaims)
