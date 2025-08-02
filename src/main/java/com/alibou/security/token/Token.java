f67092c2916474a82e239494a8c90a0da66f017c
hungnguyenvan <hung29091987@gmail.com>
2025-08-02 13:38:50 +0700

    update

--- src/main/java/com/alibou/security/token/Token.java
+++ src/main/java/com/alibou/security/token/Token.java
@@ -7,9 +7,11 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,7 +25,8 @@ import lombok.NoArgsConstructor;
 public class Token {
 
   @Id
-  @GeneratedValue
+  @SequenceGenerator(name = "token_id_seq", sequenceName = "token_id_seq", allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "token_id_seq")
   public Integer id;
 
   @Column(unique = true)
