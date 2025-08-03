2835700bc85075d8ad86b3b5266cd3ef100c7695
hungnguyenvan <hung29091987@gmail.com>
2025-08-03 23:04:09 +0700

    update

--- src/main/java/com/alibou/security/config/JwtService.java
+++ src/main/java/com/alibou/security/config/JwtService.java
@@ -18,12 +18,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class JwtService {
 
-  @Value("${application.security.jwt.secret-key}")
-  private String secretKey;
-  @Value("${application.security.jwt.expiration}")
-  private long jwtExpiration;
-  @Value("${application.security.jwt.refresh-token.expiration}")
-  private long refreshExpiration;
+  // @Value("${application.security.jwt.secret-key}")
+  private String secretKey = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+  // @Value("${application.security.jwt.expiration}")
+  private long jwtExpiration = 86400000; // a day
+  // @Value("${application.security.jwt.refresh-token.expiration}")
+  private long refreshExpiration = 604800000; // 7 days
 
   public String extractUsername(String token) {
     return extractClaim(token, Claims::getSubject);
