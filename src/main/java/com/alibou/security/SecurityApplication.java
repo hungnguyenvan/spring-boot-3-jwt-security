d93d8cfcb7bec777e86b1d3ba12b142ebc19ca91
hungnguyenvan <hung29091987@gmail.com>
2025-08-03 22:14:02 +0700

    update

--- src/main/java/com/alibou/security/SecurityApplication.java
+++ src/main/java/com/alibou/security/SecurityApplication.java
@@ -8,13 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import static com.alibou.security.user.Role.ADMIN;
 import static com.alibou.security.user.Role.EDITOR;
 
 @SpringBootApplication
-@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+//@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class SecurityApplication {
 
 	public static void main(String[] args) {
